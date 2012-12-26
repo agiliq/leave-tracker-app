498d7e844c93ce71bd1b7563ca90cfe0158a60fa
Bhaskar <bhaskar8088@gmail.com>
2012-12-26 16:48:15 +0530

    Added validation: While applying leave, end date should be after start date

--- leave_tracker_app/tracker/forms.py
+++ leave_tracker_app/tracker/forms.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from django import forms
 from django.forms import ModelForm
 from models import LeaveApplication
 
@@ -20,5 +21,15 @@ class LeaveApplicationForm(ModelForm):
             model.save()
 
         return model
+
+    def clean_end_date(self):
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+        if end_date > start_date:
+            return end_date
+        else:
+            raise forms.ValidationError("'End Date' should be after 'Start Date' .")
+
+        
 
 
