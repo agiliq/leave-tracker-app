15111f34b688e98b7a7654d137373855bb62c143
Shabda Raaj <shabda@agiliq.com>
2013-01-30 17:52:17 +0530

    Added a basic test and travis metadata.

--- leave_tracker_app/settings.py
+++ leave_tracker_app/settings.py
@@ -1,4 +1,3 @@
-# Django settings for leave_tracker_app project.
 import os
 
 
@@ -178,7 +177,22 @@ LOGGING = {
     }
 }
 
-from local_settings import *
+try:
+    from local_settings import *
+except ImportError:
+    DEBUG = True
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'dev.db',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
+    }
+
+
 TEMPLATE_DEBUG = DEBUG
 
 LEAVE_CONST = 20
