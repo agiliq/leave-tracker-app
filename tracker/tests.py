ad95b075738ac3b2e1b5d0f2973f2c6bfdf284bc
Shabda Raaj <shabda@agiliq.com>
2013-02-09 13:06:13 +0530

    Sanity tests for homepage

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,19 +1,20 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from django.test import Client
+from django.contrib.auth.models import User
 
 
 class TestViewsBasic(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="foo", email="foo@example.com",
+                    password="bar")
+        self.c = Client()
+
     def test_index(self):
         """
         Test the homepage
         """
-        c = Client()
-        response = c.get("/")
+        
+        response = self.c.get("/")
         self.assertEqual(200, response.status_code)
+        self.assertContains(response, "Login")
+        self.assertContains(response, "agiliq.com")
