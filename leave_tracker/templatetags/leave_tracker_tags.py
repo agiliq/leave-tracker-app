e7304bc761e15e6ae9750afdc254567f6f1d570f
Bhaskar <bhaskar@agiliq.com>
2013-03-17 13:03:17 +0530

    Added template tag to return leaves_taken.

---
+++ leave_tracker/templatetags/leave_tracker_tags.py
@@ -0,0 +1,18 @@
+from django.template import Library
+from django.db.models import Sum
+
+from leave_tracker.models import LeaveApplication
+
+register = Library()
+
+
+@register.filter(name='leaves_taken')
+def leaves_taken(obj):
+    leaves = LeaveApplication.objects.filter(usr=obj)
+    leaves_taken = leaves.aggregate(Sum('num_of_days'))['num_of_days__sum']
+    return leaves_taken
+
+
+
+
+
