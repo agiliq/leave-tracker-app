cb36c3ad01a9110c9d4929ba15fd59df6ee685bd
Bhaskar <bhaskar@agiliq.com>
2013-08-12 17:07:31 +0530

    Replaced exclude with fields

--- leave_tracker/forms.py
+++ leave_tracker/forms.py
@@ -11,7 +11,7 @@ class LeaveApplicationForm(ModelForm):
     class Meta:
 
         model = LeaveApplication
-        exclude = ('status', 'usr', 'num_of_days')
+        fields = ('start_date', 'end_date', 'leave_category', 'subject')
 
     def save(self, request, commit=True):
         model = super(LeaveApplicationForm, self).save(commit=False)
