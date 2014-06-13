869e37ec99fba7e8b9dd32365351381077034bca
manjunath24 <hugar.manjunathrt@gmail.com>
2014-06-13 11:21:36 +0530

    updated to work with django 1.6

--- leave_tracker/urls.py
+++ leave_tracker/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('leave_tracker.views',
     url(r'^$', 'index', name='index'),
